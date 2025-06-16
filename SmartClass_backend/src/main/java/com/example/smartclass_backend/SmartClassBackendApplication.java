9d15f8e2ec32f74993241f856a6e5da4e4c9b61f
Loo1221 <Loo1221@noreply.gitcode.com>
2025-06-16 18:20:21 +0800

    team first commit

---
+++ SmartClass_backend/src/main/java/com/example/smartclass_backend/SmartClassBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.smartclass_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartClassBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmartClassBackendApplication.class, args);
+    }
+
+}
