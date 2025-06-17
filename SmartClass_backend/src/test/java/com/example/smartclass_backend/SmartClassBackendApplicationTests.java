d5532aa77c0d6941350dc1900e9695c97513a686
Loo1221 <Loo1221@noreply.gitcode.com>
2025-06-17 21:18:00 +0800

    修正后的基础项目文件

---
+++ SmartClass_backend/src/test/java/com/example/smartclass_backend/SmartClassBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.smartclass_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartClassBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ SmartClass_frontend/src/views/HomeView.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div class="home-container">
+    <h1>欢迎使用智慧课堂系统1</h1>
+  </div>
+</template>
+
+<style scoped>
+.home-container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  font-size: 24px;
+  color: #409EFF;
+}
+</style>
